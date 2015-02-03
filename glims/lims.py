a33e35d7f872b30aa43235a273a5978207ea26d9
adam <amschaal@ucdavis.edu>
2015-02-03 15:36:57 -0800

    remove hstore references

--- glims/lims.py
+++ glims/lims.py
@@ -6,7 +6,6 @@ from django.contrib.contenttypes.models import ContentType
 from django.core.urlresolvers import reverse
 from django.db.models import Q
 from models import Plugin
-from django_hstore import hstore
 from jsonfield import JSONField
 import operator
 
--- glims/serializers.py
+++ glims/serializers.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 from glims.lims import Project, Sample, ModelType, Pool, Workflow#, File, Note
 from glims.jobs import Job, JobSubmission
 from django.contrib.auth.models import Group
-from rest_framework_hstore.serializers import HStoreSerializer
 
 from jsonfield import JSONField
 from rest_framework.fields import WritableField
