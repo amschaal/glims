3e178c33b968d1e9667d31f992bc563b4b9a9f69
Adam Schaal <amschaal@ucdavis.edu>
2015-10-16 16:56:43 -0700

    reworking some more tables with ngtable

--- glims/static/js/modals/sample_modal.js
+++ glims/static/js/modals/sample_modal.js
@@ -1,11 +1,12 @@
 angular.module('mainapp')
-.controller('SampleModalController', function SampleModalController($scope, $http, $modalInstance,addFunc,scope) {
+.controller('SampleModalController', function SampleModalController($scope, $http, DRFNgTableParams, $modalInstance,addFunc,scope) {
 	$scope.addFunc = addFunc;
 	$scope.scope = scope;
 	$scope.headers=[{'name':'created','label':'Created'},{'name':'name','label':'Name'},{'name':'project','label':'Project','order_by':'project__name'},{'name':'description','label':'Description'}];
 	$scope.permissionLink = function(sample){return django_js_utils.urls.resolve('permissions', { model: 'sample', pk: sample.id })};
 	$scope.sampleLink = function(sample){return django_js_utils.urls.resolve('sample', { pk: sample.id })};
 	$scope.projectLink = function(sample){return django_js_utils.urls.resolve('project', { pk: sample.project })};
+	$scope.tableParams = DRFNgTableParams('/api/samples/',{sorting: { created: "desc" }});
 	$scope.exists = function(record){
 		for (var i in $scope.scope){
 			if ($scope.scope[i].id==record.id)
