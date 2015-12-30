c5b9e2f507a3d8cd9fc6175437778933a905ea67
Adam Schaal <amschaal@gmail.com>
2015-12-30 11:18:24 -0800

    separate biocore project view/modify

--- bioinformatics/views.py
+++ bioinformatics/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.template.context import RequestContext
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
@@ -12,10 +12,16 @@ def bioinfo_projects(request):
 @login_required
 def bioinfo_project(request,pk):
     instance = get_object_or_404(BioinfoProject,project=pk)
+    return render(request, 'bioinformatics/bioinfo_project.html', {'instance':instance} ,context_instance=RequestContext(request))
+
+@login_required
+def modify_bioinfo_project(request,pk):
+    instance = get_object_or_404(BioinfoProject,project=pk)
     if request.method == 'POST':
         form = BioinfoProjectForm(request.POST,instance=instance)
         if form.is_valid():
             instance = form.save()
+            return redirect('bioinformatics__project',pk=instance.project.id)
     else:
         form = BioinfoProjectForm(instance=instance)
-    return render(request, 'bioinformatics/bioinfo_project.html', {'instance':instance,'form':form} ,context_instance=RequestContext(request))
+    return render(request, 'bioinformatics/modify_bioinfo_project.html', {'instance':instance,'form':form} ,context_instance=RequestContext(request))
