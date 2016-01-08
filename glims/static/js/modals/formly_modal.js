5374eea641fbeb935242f0d3a5c3e99cebca2f2e
Adam Schaal <amschaal@gmail.com>
2016-01-07 17:57:43 -0800

    more work on angular formly dialog

--- glims/static/js/modals/formly_modal.js
+++ glims/static/js/modals/formly_modal.js
@@ -8,8 +8,11 @@ angular.module('mainapp')
     });
 
   })
-.controller('FormlyModalController', function FormlyModalController($scope, $http, formlyValidationMessages, $modalInstance, fields, model) {
-	
+.controller('FormlyModalController', function FormlyModalController($scope, $http, formlyValidationMessages,formlyConfig, $modalInstance, fields, model) {
+	formlyConfig.extras.errorExistsAndShouldBeVisibleExpression = 'fc.$touched || form.$submitted';
+
+	formlyValidationMessages.addStringMessage('required', 'This field is required!');
+//	
 //	function getDRFMessage($viewValue, $modelValue, scope) {
 //		return 'there was an error';
 ////		console.log($viewValue,$modelValue,scope);
@@ -21,38 +24,65 @@ angular.module('mainapp')
 //	}
 //	formlyValidationMessages.messages.DRF = getDRFMessage;
 	
-	angular.forEach(fields,function(field,index){
-		field.validators = {
-		          DRF: {
-		              expression: function(viewValue, modelValue) {
-		            	  if (!$scope.errors)
-		            	  	return true;
-		            	  console.log('WTF????',$scope.errors[field.key] == null,viewValue,modelValue)
-		            	  return $scope.errors[field.key] == null;
-		              },
-					  message: function(){console.log('get message',$scope.errors[field.key].join(', '));return $scope.errors[field.key].join(', ');}
-//		                var value = modelValue || viewValue;
-//		                return /(\d{1,3}\.){3}\d{1,3}/.test(value);
-//		              ,message: '$viewValue + " is not a valid IP Address"'
-		            }
-		};
-		
-	});
+//	angular.forEach(fields,function(field,index){
+//		field.validators = {
+//		          DRF: {
+//		              expression: function(viewValue, modelValue) {
+//		            	  if (!$scope.errors)
+//		            	  	return true;
+//		            	  console.log('WTF????',$scope.errors[field.key] == null,viewValue,modelValue)
+//		            	  return $scope.errors[field.key] == null;
+//		              },
+//					  message: function(){console.log('get message',$scope.errors[field.key].join(', '));return $scope.errors[field.key].join(', ');}
+////		                var value = modelValue || viewValue;
+////		                return /(\d{1,3}\.){3}\d{1,3}/.test(value);
+////		              ,message: '$viewValue + " is not a valid IP Address"'
+//		            }
+//		};
+//		
+//	});
+	
+	   formlyConfig.extras.fieldTransform.push(fieldTransform);
+	   function fieldTransform(fields, model) {
+		      return fields.map(function(field) {
+		    	  if (!field.validators)
+		    		  field.validators = {};
+		    	  field.validators['server'] = {
+		    			  expression: function(viewValue, modelValue, scope) {
+		    				  if (!$scope.errors)
+			            	  	return true;
+		    				  
+			            	  console.log('WTF????',$scope.errors[field.key] == null,viewValue,modelValue)
+			            	  return $scope.errors[field.key] == null;
+			              },
+						  message: function(){console.log('get message',$scope.errors[field.key].join(', '));return $scope.errors[field.key].join(', ');}
+		    	  };
+		        return field;
+		      });
+		    }
+		        
+	
 	
 	
 	$scope.fields = fields;
 	console.log(model);
 	$scope.model = model;
 	$scope.options = {};
+	$scope.validateForm = function(){
+		angular.forEach($scope.fields,function(field,index){
+			field.formControl.$validate();
+		});
+	}
 	$scope.onSubmit = function() {
-//	      alert(JSON.stringify($scope.model), null, 2);
 	      $scope.model.$save(
-	    		  function(){$modalInstance.close($scope.model);},
+	    		  function(){
+//	    			  $scope.errors = null;
+//	    			  $scope.validateForm();
+	    			  $modalInstance.close($scope.model);
+	    		  },
 	    		  function(data){
 	    			  $scope.errors = data.data;
-//	    			  angular.forEach(data.data,function(errors,key){
-//	    				  $scope.form.$setValidity(key,false);  
-//	    			  });
+	    			  $scope.validateForm();
 	    		  }
 	      );
 	    }
