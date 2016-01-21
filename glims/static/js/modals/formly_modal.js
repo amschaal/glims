7c6210a7933dca44bbe0e473cc8a25803ba703b0
Adam Schaal <amschaal@ucdavis.edu>
2016-01-21 15:34:31 -0800

    organising angular modules

--- glims/static/js/modals/formly_modal.js
+++ glims/static/js/modals/formly_modal.js
@@ -1,4 +1,4 @@
-angular.module('mainapp')
+angular.module('formly.modal',[])
 .config(function (formlyConfigProvider) {
 
     formlyConfigProvider.setWrapper({
@@ -8,123 +8,12 @@ angular.module('mainapp')
     });
 
   })
-.controller('FormlyModalController', function FormlyModalController($scope, $http, formlyValidationMessages,formlyConfig, $modalInstance, fields, model, options) {
-	formlyConfig.extras.errorExistsAndShouldBeVisibleExpression = 'fc.$touched || form.$submitted';
-	formlyConfig.setType({
-        name: 'ui-select-multiple',
-        extends: 'select',
-        templateUrl: 'ui-select-multiple.html'
-      });
-	formlyConfig.setType({
-        name: 'ui-select-multiple-search',
-        extends: 'select',
-        templateUrl: 'ui-select-multiple-search.html'
-      });
-	formlyConfig.setType({
-        name: 'objectMultiCheckbox',
-//        extends: 'multiCheckbox',
-        templateUrl: 'object-multi-checkbox.html',
-        wrapper: ['bootstrapLabel', 'bootstrapHasError'],
-        controller: /* @ngInject */["$scope", function controller($scope) {
-        	var to = $scope.to;
-	        var opts = $scope.options;
-	        $scope.multiCheckbox = {
-	  	          checked: []
-//	  	          change: setModel
-	  	        };
-	        function checkBoxes(selected,options){
-	        	var ids = selected.map(function(obj){
-	        		return obj[to.valueProp];
-	        	});
-	        	angular.forEach(options,function(option,index){
-	        		$scope.multiCheckbox.checked[index] = ids.indexOf(option[to.valueProp]) !== -1;
-	        	});
-	        }
-	        checkBoxes($scope.model[opts.key],to.options);
-	        $scope.multiCheckbox.change = function() {
-	        	console.log($scope.multiCheckbox.checked);
-	        	var selected = [];
-		          angular.forEach($scope.multiCheckbox.checked, function (checkbox, index) {
-		            if (checkbox) {
-		              selected.push(to.options[index]);
-		            }
-		          });
-		          $scope.model[opts.key] = selected;
-		          // Must make sure we mark as touched because only the last checkbox due to a bug in angular.
-		          $scope.fc.$setTouched();
-	        }
-        }]
-      });
+.controller('FormlyModalController', function FormlyModalController($scope, $http, $modalInstance, fields, model, options) {
+	
 	
 	$scope.title = options.title ? options.title : 'Edit';
 
-//	formlyValidationMessages.addStringMessage('required', 'This field is required!');
-//	formlyValidationMessages.messages.server= getServerMessage;
-//
-//	function getServerMessage($viewValue, $modelValue, scope) {
-//		console.log('server message');
-//		return 'wtf';
-//	  if (scope.data.useGenericMessage) {
-//	    return 'This field is required';
-//	  } else {  
-//	    return scope.to.label + ' is required';
-//	  }
-//	}
-//	
-//	function getDRFMessage($viewValue, $modelValue, scope) {
-//		return 'there was an error';
-////		console.log($viewValue,$modelValue,scope);
-//////	  if ($scope.errors.data.useGenericMessage) {
-//////	    return 'This field is required';
-//////	  } else {  
-//////	    return scope.to.label + ' is required';
-//////	  }
-//	}
-//	formlyValidationMessages.messages.DRF = getDRFMessage;
-	
-//	angular.forEach(fields,function(field,index){
-//		field.validators = {
-//		          DRF: {
-//		              expression: function(viewValue, modelValue) {
-//		            	  if (!$scope.errors)
-//		            	  	return true;
-//		            	  console.log('WTF????',$scope.errors[field.key] == null,viewValue,modelValue)
-//		            	  return $scope.errors[field.key] == null;
-//		              },
-//					  message: function(){console.log('get message',$scope.errors[field.key].join(', '));return $scope.errors[field.key].join(', ');}
-////		                var value = modelValue || viewValue;
-////		                return /(\d{1,3}\.){3}\d{1,3}/.test(value);
-////		              ,message: '$viewValue + " is not a valid IP Address"'
-//		            }
-//		};
-//		
-//	});
 	   fields = angular.copy(fields);
-//	   formlyConfig.extras.fieldTransform.push(fieldTransform);
-//	   function fieldTransform(fields, model) {
-//		      return fields.map(function(field) {
-//		    	  if (!field.validators)
-//		    		  field.validators = {};
-//		    	  var error_key = field.key;
-//		    	  if (field.data)
-//		    		  error_key = field.data.error_key ? field.data.error_key : field.key;
-//		    	  
-//		    	  field.validators['server'] = {
-//		    			  expression: function(viewValue, modelValue, scope) {
-//		    				  console.log('error key',error_key,$scope.errors[error_key] == null,$scope.errors[error_key]);
-//		    				  if (!$scope.errors)
-//			            	  	return true;
-//			            	  return $scope.errors[error_key] == null;
-//			              },
-//						  message: function()
-//						  {		
-//							  	console.log('get message',$scope.errors[error_key].join(', '));
-//						  		return $scope.errors[error_key].join(', ');
-//						  }
-//		    	  };
-//		        return field;
-//		      });
-//		    }
 	   angular.forEach(fields,function(field,index){
 	   if (!field.validators)
     		  field.validators = {};
--- glims/static/js/services/glims_services.js
+++ glims/static/js/services/glims_services.js
@@ -1,28 +1,65 @@
 angular.module('glimsServices',['ngTable'])
 .factory('DRFNgTableParams', ['NgTableParams','$http', function(NgTableParams,$http) {
-	   return function(url,ngparams) {
-		   var params = {
-//				    page: 1, // show first page
-//				    filter:{foo:'bar'}, //filter stuff
-				    count: 10 // count per page
-				  }
-		   angular.merge(params,ngparams);
-		   return new NgTableParams(params, {
-			    filterDelay: 0,
-				  	getData: function(params) {
-				  		var url_params = params.url();
-				  		console.log(params);
-				  		console.log(url);
-				  	var query_params = {page:url_params.page,page_size:url_params.count,ordering:params.orderBy().join(',').replace('+','')};
-				  	angular.extend(query_params, params.filter());
-			      // ajax request to api
-				  	return $http.get(url,{params:query_params}).then(function(response){
-				  		console.log(response.data);
-				  		params.total(response.data.count);
-				  		return response.data.results;
-				  	});
-			    }
-			  });
-	   };
-	 }]);
+	return function(url,ngparams) {
+		var params = {
+//				page: 1, // show first page
+//				filter:{foo:'bar'}, //filter stuff
+				count: 10 // count per page
+		}
+		angular.merge(params,ngparams);
+		return new NgTableParams(params, {
+			filterDelay: 0,
+			getData: function(params) {
+				var url_params = params.url();
+				console.log(params);
+				console.log(url);
+				var query_params = {page:url_params.page,page_size:url_params.count,ordering:params.orderBy().join(',').replace('+','')};
+				angular.extend(query_params, params.filter());
+				// ajax request to api
+				return $http.get(url,{params:query_params}).then(function(response){
+					console.log(response.data);
+					params.total(response.data.count);
+					return response.data.results;
+				});
+			}
+		});
+	};
+}])
+ .service('cartService', function($rootScope,$http) {
+	 return {
+		 setSamples: setSamples,
+		 getSamples: getSamples,
+		 removeSamples: removeSamples,
+		 addSamples: addSamples
+	 };
+	 var samples = [];
+	 function setSamples(data) {
+		 console.log('set',data);
+		 samples = data;
+		 $rootScope.$broadcast('cart',data);
+	 }
+
+	 function getSamples(){
+		 console.log('get',samples);
+		 return samples;
+	 }
+	 function removeSamples(sample_ids){
+		 var url = django_js_utils.urls.resolve('remove_samples_from_cart');
+		 $http.post(url,{sample_ids:sample_ids})
+		 .success(function(data, status, headers, config) {
+			 setSamples(data);
+		 }).error(function(){
+		 });
+	 };
+	 function addSamples(sample_ids){
+		 var url = django_js_utils.urls.resolve('add_samples_to_cart');
+		 $http.post(url,{sample_ids:sample_ids})
+		 .success(function(data, status, headers, config) {
+			 setSamples(data);
+		 }).error(function(){
+		 });
+	 };
+
+
+ });
 
