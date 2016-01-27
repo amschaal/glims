493d3f0e42ce778d41e27516e6586675ad007e1a
Adam Schaal <amschaal@ucdavis.edu>
2016-01-26 17:38:47 -0800

    A little cleanup

--- glims/static/js/pages/project.js
+++ glims/static/js/pages/project.js
@@ -76,12 +76,7 @@ function ProjectController($scope , $log, FormlyModal, ModelType, Project){
     };
 }
 function SamplesController($scope,$http,$log,$uibModal,FormlyModal,$Sample) {
-	var sampleDefaults;
 	$scope.errors = false;
-	$scope.setSampleDefaults = function(defaults){
-		sampleDefaults = defaults;
-		
-	};
 	$scope.save = function(sample){
 		$log.info('save',sample);
 		var onSuccess = function(response){delete sample.errors;};
@@ -90,12 +85,6 @@ function SamplesController($scope,$http,$log,$uibModal,FormlyModal,$Sample) {
 			sample.$save(onSuccess,onError);
 		else
 			sample.$create(onSuccess,onError);
-	};
-	$scope.addSample = function(){
-//		var sample = new $Sample(sampleDefaults);
-//		$scope.samples.push(sample);
-//		$log.info('sample',sample);
-		$scope.edit_sample();
 	};
 	$scope.cancel = function(sample,index){
 		sample.editing=false;
@@ -130,11 +119,6 @@ function SamplesController($scope,$http,$log,$uibModal,FormlyModal,$Sample) {
         })
         .success(function(data){
         	$scope.errors = false;
-//        	for (var i in data){
-//        	var sample = new $Sample(data[i]);
-//	    		sample.editing=true;
-//	    		$scope.samples.push(sample);	
-//        	}
         	$scope.refreshSamples();
         })
         .error(function(data){
@@ -147,21 +131,10 @@ function SamplesController($scope,$http,$log,$uibModal,FormlyModal,$Sample) {
 		$scope.refreshSamples();
 	};
 	$scope.refreshSamples = function(){
-		var params = angular.copy(sampleDefaults);
-		params.page_size=100;
-		$scope.samples = $Sample.query(params,function() {
-			angular.forEach($scope.samples,function(sample){
-				//$scope.addNote(note);
-			});
-			$log.info('SAMPLES!',$scope.samples);
+		$scope.samples = $Sample.query({project:$scope.project.id},function() {
 			$scope.gridOptions.data = $scope.samples;
 		});
 	}
-//	$scope.setFields = function(fields){
-//		angular.forEach(fields,function(field){
-//			$scope.gridOptions.columnDefs.push({displayName:field.label,name:'data.'+field.name,minWidth: 100})
-//		});
-//	};
 	var columnDefs = [
 	                  {
 	                	  	displayName: "Actions",
@@ -200,8 +173,6 @@ function SamplesController($scope,$http,$log,$uibModal,FormlyModal,$Sample) {
 				name:'data.'+field.name,
 				minWidth: 100,
 				cellTemplate: '<div class="ui-grid-cell-contents"><div ng-if="grid.appScope.project.sample_type.id != row.entity.type.id" class="error">Incompatible type!</div>{{COL_FIELD}}</div>'
-//				cellTemplate: '\
-//					<div>{[row]}</div>',
 			});
 		});
 		$scope.gridOptions.columnDefs = cols;
@@ -220,38 +191,13 @@ function SamplesController($scope,$http,$log,$uibModal,FormlyModal,$Sample) {
 	  };
 
 
-//	  $scope.edit_sample = function (row) {
-//		  $log.info(row);
-//		    var modalInstance = $uibModal.open({
-//		      templateUrl: 'SampleEdit.html',
-//		      controller: 'SampleEditCtrl',
-//		      size: 'lg',
-//		      resolve: {
-//		        sample: function () {
-//		        	if (row)
-//		        		return angular.copy(row.entity);
-//		        	else
-//		        		return new $Sample(sampleDefaults);
-//		        }
-//		      }
-//		    });
-//
-//		    modalInstance.result.then(function (sample) {
-//		    	if (row)
-//		    		row.entity = sample;
-//		    	else
-//		    		$scope.samples.push(sample);
-//		    }, function () {
-//		      $log.info('Modal dismissed at: ' + new Date());
-//		    });
-//		  };
 		  var fields = [
 		                {"templateOptions": {"required": false, "description": "", "label": "Name"}, "type": "input", "key": "name"}, 
 		                {"templateOptions": {"required": false, "description": "", "label": "Description"}, "type": "textarea", "key": "description"},
 		                {"templateOptions": {"required": false, "description": "", "label": "Received"}, "type": "input", "key": "received"}
 		                ]
 		  $scope.edit_sample = function (row) {
-		    	var sample = row ? row.entity : new $Sample({project:$scope.project.id,type:$scope.project.sample_type});
+		    	var sample = row ? row.entity : new $Sample({project:$scope.project.id,type:$scope.project.sample_type,data:{}});
 			  	FormlyModal.create(fields,sample,{model_type_query:{content_type__model:'sample'},title:'Edit Sample',controller:'ExtendedFormlyModalController'})
 		    	.result.then(
 		    			function (updatedSample) {
@@ -266,29 +212,3 @@ function SamplesController($scope,$http,$log,$uibModal,FormlyModal,$Sample) {
 	
 }
 
-app.controller('SampleEditCtrl', function ($scope,$log, $uibModalInstance, sample) {
-		$log.info(sample);
-	  $scope.sample = sample;
-
-	  $scope.ok = function () {
-	    $scope.save();
-	  };
-	  $scope.save = function(){
-			$log.info('save',$scope.sample);
-			var onSuccess = function(response){delete sample.errors;$uibModalInstance.close(sample);};
-			var onError = function(response){sample.errors = response.data;$log.info(response.data);};
-			if(sample.id)
-				sample.$save(onSuccess,onError);
-			else
-				sample.$create(onSuccess,onError);
-		};
-	  $scope.getErrors = function(field){
-		  if (!$scope.sample.errors)
-			  return [];
-		  else
-			  return $scope.sample.errors[field];
-	  }
-	  $scope.cancel = function () {
-	    $uibModalInstance.dismiss('cancel');
-	  };
-	});
