db44aedfe3ec47a2d3d075bd022fd901daa195c1
Adam Schaal <amschaal@ucdavis.edu>
2016-06-06 17:06:07 -0700

    Add URL field to Lab

--- glims/lims.py
+++ glims/lims.py
@@ -58,6 +58,7 @@ class MyModelExtended(MyModel):
 class Lab(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
+    url = models.URLField(blank=True,null=True)
     slug = models.SlugField(max_length=20,unique=True,null=True)
 #     cloudstore = models.ForeignKey(CloudStore,null=True,blank=True,on_delete=models.SET_NULL)
 #     def create_cloudstore(self):
