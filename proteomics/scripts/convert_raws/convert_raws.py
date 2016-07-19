076da5cd18185151d9233a08ba88deefbe92d04d
Adam Schaal <amschaal@gmail.com>
2016-07-19 11:38:56 -0700

    Add file upload to proteomics raw file conversion.

--- proteomics/scripts/convert_raws/convert_raws.py
+++ proteomics/scripts/convert_raws/convert_raws.py
@@ -1,4 +1,9 @@
-import argparse, glob
+#REQUIRES Poster library for posting files: https://atlee.ca/software/poster/
+import poster.encode
+import poster.streaminghttp
+
+
+import argparse, glob, os
 import json
 import urllib2, urllib
 RAW_DIRECTORY = '/data/raws'
@@ -13,26 +18,40 @@ def parse_sample_id(filename):
     # @todo: Make this more robust
     return filename.split('_')[0]
 
-def get_sample(sample_id):
-#     params = urllib.urlencode({'sample_id': sample_id})
-    response = urllib2.urlopen(urllib2.Request(SAMPLE_API_URL%sample_id,headers={'Authorization': 'Token %s'%AUTHENTICATION_TOKEN}))
+def get_sample(sample_id,options):
+    response = urllib2.urlopen(urllib2.Request(SAMPLE_API_URL%sample_id,headers={'Authorization': 'Token %s'%options.auth_token}))
     data = json.loads(response.read())
 #     except urllib2.HTTPError as e:
 #         error_message = e.read()
 #         print error_message
     return data
 
+def post_sample_file(sample_id,file_path,subdir,options):
+    try:
+        opener = poster.streaminghttp.register_openers()
+        params = {'file': open(file_path,'rb'), 'subdir': subdir}
+        datagen, headers = poster.encode.multipart_encode(params)
+        headers['Authorization'] = 'Token %s'%options.auth_token
+        response = opener.open(urllib2.Request(UPLOAD_SAMPLE_FILE_URL%sample_id, datagen, headers))
+        data = json.loads(response.read())
+        return data
+    except Exception as e:
+        print e.read()
+
 def convert_files(options):
     raws = glob.glob1(options.directory, '*.raw')
     for raw in raws:
         sample_id = parse_sample_id(raw)
-        print get_sample(sample_id)
+        print get_sample(sample_id,options)
+        file_path = os.path.join(options.directory,raw)
+        print post_sample_file(sample_id, file_path, 'mzmls', options)
         
 def main():
     parser = argparse.ArgumentParser(description='Convert raw files to MZML format and upload to GLIMS')
 #     parser.add_argument('--mzml', required=True, nargs='+', help='An mzML file path.  May use wildcards to use multiple files, such as *.mzml')
     parser.add_argument('--directory', required=False, default=RAW_DIRECTORY, help='The directory containing the raw files.')
     parser.add_argument('--processed_directory', required=False, default=PROCESSED_DIRECTORY, help='Where to put the raw files after conversion.')
+    parser.add_argument('--auth_token', required=False, default=AUTHENTICATION_TOKEN, help='Authentication token for connecting to REST services.')
     options = parser.parse_args()
     convert_files(options)
     
