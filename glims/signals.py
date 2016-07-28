d5776e4680730ec395c40506f86457d7513ef360
Adam Schaal <amschaal@ucdavis.edu>
2016-07-27 17:37:44 -0700

    forgot to add signals.py

---
+++ glims/signals.py
@@ -0,0 +1,10 @@
+from django.dispatch import Signal
+object_updated = Signal(providing_args=["instance","old_instance"])
+def object_updated_callback(sender,instance,**kwargs):
+    if not hasattr(instance, 'id'):
+        return
+    try:
+        old_instance = sender.objects.get(id=instance.id)
+        object_updated.send(sender,instance=instance,old_instance=old_instance)
+    except sender.DoesNotExist, e:
+        pass
