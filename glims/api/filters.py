a513a5533bda0f17dea70ab582ce0b398b6e8362
Adam Schaal <amschaal@ucdavis.edu>
2017-01-09 13:40:12 -0800

    Fix Following filter so that it includes participants.  Closes #34

--- glims/api/filters.py
+++ glims/api/filters.py
@@ -1,18 +1,8 @@
 from rest_framework import filters
 from django.db.models.query_utils import Q
 import operator
+from glims.models import Project
 
-
-
-# class UserSubscription(models.Model):
-#     user = models.ForeignKey(User,related_name='user_subscriptions')
-#     content_type = models.ForeignKey(ContentType)
-#     object_id = models.CharField(max_length=30) #Can be coerced into integer key if necessary
-#     content_object = GenericForeignKey('content_type', 'object_id')
-#     subscribed = models.BooleanField(default=True)
-#     email = models.BooleanField(default=True)
-#     class Meta:
-#         unique_together = (("user", "content_type", "object_id"),)
 
 class FollowingProjectFilter(filters.BaseFilterBackend):
     """
@@ -22,21 +12,16 @@ class FollowingProjectFilter(filters.BaseFilterBackend):
         following = view.request.query_params.get('following',None)
         if not following:
             return queryset
+        project_ids = [int(id) for id in Project.objects.filter(participants__id=request.user.id).values_list('id',flat=True)]
         clauses = [Q(manager=request.user)]#,Q(participants__id=request.user.id)
-        try:
-            from notifications.models import UserSubscription
-            from django.contrib.contenttypes.models import ContentType
-            from glims.models import Project
-            ct = ContentType.objects.get_for_model(Project)
-            project_ids = UserSubscription.objects.filter(user=request.user,content_type=ct,subscribed=True).values_list('object_id',flat=True)
-            print project_ids
-            clauses.append(Q(id__in=[int(id) for id in project_ids])) 
-        except Exception, e:
-            print e.message
-            pass
-        print 'filtering'
+#         try:
+        from notifications.models import UserSubscription
+        from django.contrib.contenttypes.models import ContentType
+        ct = ContentType.objects.get_for_model(Project)
+        project_ids += [int(id) for id in UserSubscription.objects.filter(user=request.user,content_type=ct,subscribed=True).values_list('object_id',flat=True)]
+        clauses.append(Q(id__in=project_ids)) 
+#         except Exception, e:
+#             pass
         query = reduce(operator.or_,clauses)
-#         print query
         queryset =  queryset.filter(query)
-#         print queryset.query
         return queryset 
